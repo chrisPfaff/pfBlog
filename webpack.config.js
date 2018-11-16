6f8c4628d50c6b7711df8c0f859d3384b753b7b7
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-16 12:31:55 -0600

    changed server.js to be able to host on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const outputDirectory = "dist";
 require("@babel/polyfill");
 
 module.exports = {
+  mode: "production",
   entry: ["@babel/polyfill", "./src/client/index.js"],
   output: {
-    path: path.join(__dirname, outputDirectory),
+    path: __dirname,
     filename: "bundle.js",
     publicPath: "/"
   },
@@ -36,7 +36,6 @@ module.exports = {
     historyApiFallback: true
   },
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: "./public/index.html"
     })
