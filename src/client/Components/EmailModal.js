1b56be3f87680b92072067e1d7d7326835d82b18
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-17 20:01:20 -0600

    hooked up emailjs to send emails

--- src/client/Components/EmailModal.js
+++ src/client/Components/EmailModal.js
@@ -2,14 +2,37 @@ import React, { Component } from "react";
 import "../styles/emailmodal.scss";
 const emailjs = require("emailjs-com");
 
+//"gmail.com"
+//template_xnRFI8hB
+//user id // user_IphnBP661H4pspiSgLyAf
+
 //emailjs.sendForm(serviceID, templateID, form, userID);
+
+//emailjs.send("gmail", "template_xnRFI8hB", {"name":"greg@gmail.com","message":"call //me asap"})
 
 const EmailModal = ({ handleClose, show }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
-  const sendEmail = query => {
-    query.preventDefault();
-    console.log(query.target.email.value);
-    console.log(query.target.text.value);
+
+  const sendEmail = e => {
+    e.preventDefault();
+    let name = e.target.name.value;
+    let text = e.target.message.value;
+
+    emailjs
+      .send(
+        "gmail",
+        "template_xnRFI8hB",
+        { name: name, message: text },
+        "user_IphnBP661H4pspiSgLyAf"
+      )
+      .then(
+        function(response) {
+          alert("Message Sent");
+        },
+        function(error) {
+          console.log("FAILED...", error);
+        }
+      );
   };
 
   return (
@@ -17,12 +40,12 @@ const EmailModal = ({ handleClose, show }) => {
       <section className="modal-main">
         <form onSubmit={sendEmail} className="emailForm">
           <label htmlFor="email">Contact Email: </label>
-          <input type="email" name="email" size="14" required />
+          <input type="email" name="name" size="14" required />
 
           <label htmlFor="text">Message: </label>
           <input
             type="text"
-            name="text"
+            name="message"
             minLength="0"
             maxLength="30"
             size="40"
