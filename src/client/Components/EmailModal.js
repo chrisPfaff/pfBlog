8d3bc4898fe8d3ecce524f9936718a840523f652
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-19 11:19:08 -0600

    moved email function outside render to see if the bug stops

--- src/client/Components/EmailModal.js
+++ src/client/Components/EmailModal.js
@@ -12,28 +12,44 @@ const emailjs = require("emailjs-com");
 
 const EmailModal = ({ handleClose, show }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
+  emailjs
+    .send(
+      "gmail",
+      "template_xnRFI8hB",
+      { name: "heelo", message: "works" },
+      "user_IphnBP661H4pspiSgLyAf"
+    )
+    .then(
+      function(response) {
+        alert("Message Sent");
+        handleClose();
+      },
+      function(error) {
+        console.log("FAILED...", error);
+      }
+    );
 
   const sendEmail = e => {
     console.log(e);
     let name = e.target.name.value;
     let text = e.target.message.value;
 
-    emailjs
-      .send(
-        "gmail",
-        "template_xnRFI8hB",
-        { name: name, message: text },
-        "user_IphnBP661H4pspiSgLyAf"
-      )
-      .then(
-        function(response) {
-          alert("Message Sent");
-          handleClose();
-        },
-        function(error) {
-          console.log("FAILED...", error);
-        }
-      );
+    // emailjs
+    //   .send(
+    //     "gmail",
+    //     "template_xnRFI8hB",
+    //     { name: 'heelo', message: 'works' },
+    //     "user_IphnBP661H4pspiSgLyAf"
+    //   )
+    //   .then(
+    //     function(response) {
+    //       alert("Message Sent");
+    //       handleClose();
+    //     },
+    //     function(error) {
+    //       console.log("FAILED...", error);
+    //     }
+    //   );
     e.preventDefault();
   };
 
