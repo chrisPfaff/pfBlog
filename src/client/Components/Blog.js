2004c7aae6cbfb7793cce015fe0aa77e093511fa
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-23 12:02:53 -0600

    added to blog about classes

--- src/client/Components/Blog.js
+++ src/client/Components/Blog.js
@@ -33,8 +33,11 @@ class Blog extends Component {
               than older methods. In this example above instead of using a
               constructor function one can create a class with using the class
               keyword. Like a constructor function you still have to use the
-              this keyword to assign properties to the whatever variable is
-              passed in the constructor.
+              "this" keyword to assign properties to the whatever variable is
+              passed in the constructor. Unlike in other OOP languages Js
+              classes are not true classes. In other languages you can create a
+              copy of classes but in Js when you instantiate a new Object off of
+              a class it creates an object that is linked by the prototype.
             </p>
           </section>
           <h2 className="blogDate">Nov 10 2018</h2>
