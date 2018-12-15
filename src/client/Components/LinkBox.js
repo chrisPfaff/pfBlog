c5ca2fa03a0139c463dd9f6fc1279db14bb9ac5d
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-15 12:44:50 -0600

    changed link styles to match about card style

--- src/client/Components/LinkBox.js
+++ src/client/Components/LinkBox.js
@@ -9,17 +9,23 @@ class LinkBox extends Component {
     console.log(this.props);
     return (
       <div className="linkBox">
-        <li>
-          <a href={this.props.data[1]}>
-            <img className="linkImg" src={this.props.data[0]} alt="alt image" />
-            <div>
-              <span>{this.props.data[3]}</span>
-            </div>
-            <div>
-              <span>{this.props.data[2]}</span>
-            </div>
-          </a>
-        </li>
+        <ol>
+          <li>
+            <a href={this.props.data[1]}>
+              <img
+                className="linkImg"
+                src={this.props.data[0]}
+                alt="alt image"
+              />
+              <div>
+                <span>{this.props.data[3]}</span>
+              </div>
+              <div>
+                <span>{this.props.data[2]}</span>
+              </div>
+            </a>
+          </li>
+        </ol>
       </div>
     );
   }
