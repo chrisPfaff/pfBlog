bb5f3bc0c3898999120582b65981188460b47a84
chrisPfaff <chrispfaff10@gmail.com>
2026-03-06 22:09:07 -0600

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 import Landing from "./Components/Landing";
+import NotFound from "./Components/NotFound";
 
 function App() {
-  return <Landing />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
